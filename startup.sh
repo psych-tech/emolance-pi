5a067e33a525abdf288c2c7a378207ad12f40241
Yu Sun <yu.sun.cs@gmail.com>
2015-06-22 20:24:10 -0700

    disable gpio input

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 
-sudo python /home/pi/emolance-pi/control-scripts/inputtest.py &
+#sudo python /home/pi/emolance-pi/control-scripts/inputtest.py &
 
 /usr/bin/nohup /usr/local/bin/node /home/pi/emolance-pi/node-server/app.js &
